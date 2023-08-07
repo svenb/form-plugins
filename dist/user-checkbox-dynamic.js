907665b00f628e66e56c5b90929e1837fc63f462
Sven Berling <ich@sven-berling.de>
2023-08-07 15:59:43 +0200

    Chnages to typescript

--- dist/user-checkbox-dynamic.js
+++ dist/user-checkbox-dynamic.js
@@ -1,4 +1,4 @@
-import { _ as _decorate, s, i, a as _taggedTemplateLiteral, x, e as e$1, d as _inherits, f as _createSuper, g as _createClass, j as _classCallCheck, k as _assertThisInitialized, l as _asyncToGenerator, m as _regeneratorRuntime } from './query-assigned-elements-f61280a9.js';
+import { _ as _decorate, s, i, a as _taggedTemplateLiteral, x, e as e$1, d as _inherits, f as _createSuper, g as _createClass, j as _classCallCheck, k as _assertThisInitialized } from './query-assigned-elements-f61280a9.js';
 import { e } from './property-3bc85059.js';
 
 var config = {
@@ -11,38 +11,29 @@ var config = {
     DynamicCheckBoxVal: {
       type: 'string',
       title: 'Data Variable',
-      description: 'Data Variable'
+      description: 'Data Variable',
+      required: true
     },
     DataStructure: {
       type: 'string',
-      title: 'Data Structure',
-      description: 'Data Structure'
+      title: 'Select column in your Table/DSV/JSON',
+      description: 'Please choose the column, you would like to see as your Checkboxes',
+      required: true
     }
   },
   standardProperties: {
-    fieldLabel: false,
+    fieldLabel: true,
     description: false,
     defaultValue: false,
     readOnly: false
   }
 };
 
-var _templateObject, _templateObject2, _templateObject3;
-
-/*interface Checkbox {
-  [key: string]: unknown;
-}*/
+var _templateObject, _templateObject2;
 var CustomCheckbox = _decorate([e$1('user-checkbox-dynamic')], function (_initialize, _LitElement) {
   var CustomCheckbox = /*#__PURE__*/function (_LitElement2) {
     _inherits(CustomCheckbox, _LitElement2);
     var _super = _createSuper(CustomCheckbox);
-    /* private checkboxes: Checkbox[] = [
-      { id: 'checkbox1', label: 'Checkbox 1', checked: true },
-      { id: 'checkbox2', label: 'Checkbox 2', checked: false },
-      { id: 'checkbox3', label: 'Checkbox 3', checked: true },
-    ];
-    */
-
     function CustomCheckbox() {
       var _this;
       _classCallCheck(this, CustomCheckbox);
@@ -57,6 +48,20 @@ var CustomCheckbox = _decorate([e$1('user-checkbox-dynamic')], function (_initia
     F: CustomCheckbox,
     d: [{
       kind: "field",
+      decorators: [e({
+        type: String
+      })],
+      key: "DynamicCheckBoxVal",
+      value: void 0
+    }, {
+      kind: "field",
+      decorators: [e({
+        type: String
+      })],
+      key: "DataStructure",
+      value: void 0
+    }, {
+      kind: "field",
       "static": true,
       key: "getMetaConfig",
       value: function value() {
@@ -66,63 +71,40 @@ var CustomCheckbox = _decorate([e$1('user-checkbox-dynamic')], function (_initia
       }
     }, {
       kind: "field",
-      decorators: [e({
-        type: String
-      })],
-      key: "DynamicCheckBoxVal",
-      value: function value() {
-        return '[{"Name":"Rückmeldung","anzeige":true,"ID":1}]';
-      }
-    }, {
-      kind: "field",
       "static": true,
       key: "styles",
       value: function value() {
         return i(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    .checkbox-container {\n      display: flex;\n      flex-direction: column;\n    }\n\n    .checkbox-item {\n      display: flex;\n      align-items: center;\n      margin-bottom: 5px;\n    }\n\n    .checkbox-input {\n      margin-right: 5px;\n    }\n\n    .checkbox-label {\n      font-size: 14px;\n    }\n  "])));
       }
     }, {
+      kind: "field",
+      key: "DynamicCheckJson",
+      value: function value() {
+        return [];
+      }
+    }, {
       kind: "method",
       key: "loadCheckboxes",
-      value: function () {
-        var _loadCheckboxes = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee() {
-          var DynamicCheboxJson;
-          return _regeneratorRuntime().wrap(function _callee$(_context) {
-            while (1) switch (_context.prev = _context.next) {
-              case 0:
-                _context.prev = 0;
-                _context.next = 3;
-                return JSON.parse(this.DynamicCheckBoxVal);
-              case 3:
-                DynamicCheboxJson = _context.sent;
-                console.log(DynamicCheboxJson);
-                // const response = await fetch('/checkboxes.json'); // Adjust the path if needed
-                // this.checkboxes = await response.json();
-                this.requestUpdate();
-                _context.next = 12;
-                break;
-              case 8:
-                _context.prev = 8;
-                _context.t0 = _context["catch"](0);
-                console.error('Error loading checkboxes:', _context.t0);
-                console.error(this.DynamicCheckBoxVal);
-              case 12:
-              case "end":
-                return _context.stop();
-            }
-          }, _callee, this, [[0, 8]]);
-        }));
-        function loadCheckboxes() {
-          return _loadCheckboxes.apply(this, arguments);
+      value: function loadCheckboxes() {
+        // if (!this.DynamicCheckBoxVal) {
+        //  return;
+        // }
+        try {
+          this.DynamicCheckJson = JSON.parse(this.DynamicCheckBoxVal);
+          this.DynamicCheckJson.forEach(function (element) {
+            console.log(element.Name);
+          });
+          this.requestUpdate();
+        } catch (error) {
+          console.error('Error loading checkboxes:', error);
+          console.log(this.DynamicCheckBoxVal);
         }
-        return loadCheckboxes;
-      }()
+      }
     }, {
       kind: "method",
       key: "render",
       value: function render() {
-        return x(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n      ", "\n      <div class=\"checkbox-container\">\n        ", "\n      </div>\n    "])), this.DynamicCheckBoxVal, this.checkboxes.map(function (checkbox) {
-          return x(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n            <div class=\"checkbox-item\">\n              <input type=\"checkbox\" class=\"checkbox-input\" id=\"", "\" .checked=\"", "\" />\n              <label class=\"checkbox-label\" for=\"", "\">", "</label>\n            </div>\n          "])), checkbox.id, checkbox.checked, checkbox.id, checkbox.label);
-        }));
+        return x(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral([" <b>", "</b> "])), this.DataStructure);
       }
     }]
   };
--- src/components/user-checkbox-dynamic/user-checkbox-dynamic.ts
+++ src/components/user-checkbox-dynamic/user-checkbox-dynamic.ts
@@ -3,15 +3,19 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { config } from './user-checkbox-dynamic.config';
 
-/*interface Checkbox {
-  [key: string]: unknown;
-}*/
+interface DynamicJson {
+  [key: string]: string;
+}
+
 @customElement('user-checkbox-dynamic')
 export class CustomCheckbox extends LitElement {
-  static getMetaConfig = (): Promise<PluginContract> | PluginContract => config;
+  @property({ type: String })
+  DynamicCheckBoxVal!: string;
 
   @property({ type: String })
-  DynamicCheckBoxVal = '[{"Name":"Rückmeldung","anzeige":true,"ID":1}]';
+  DataStructure!: string;
+
+  static getMetaConfig = (): Promise<PluginContract> | PluginContract => config;
 
   static styles = css`
     .checkbox-container {
@@ -34,44 +38,31 @@ export class CustomCheckbox extends LitElement {
     }
   `;
 
-  /* private checkboxes: Checkbox[] = [
-    { id: 'checkbox1', label: 'Checkbox 1', checked: true },
-    { id: 'checkbox2', label: 'Checkbox 2', checked: false },
-    { id: 'checkbox3', label: 'Checkbox 3', checked: true },
-  ];
-  */
-
   constructor() {
     super();
     this.loadCheckboxes();
   }
 
-  private async loadCheckboxes() {
+  DynamicCheckJson: DynamicJson[] = [];
+
+  loadCheckboxes() {
+    // if (!this.DynamicCheckBoxVal) {
+    //  return;
+    // }
     try {
-      let DynamicCheboxJson = await JSON.parse(this.DynamicCheckBoxVal);
-      console.log(DynamicCheboxJson);
-      // const response = await fetch('/checkboxes.json'); // Adjust the path if needed
-      // this.checkboxes = await response.json();
+      this.DynamicCheckJson = JSON.parse(this.DynamicCheckBoxVal) as DynamicJson[];
+      this.DynamicCheckJson.forEach((element) => {
+        console.log(element.Name);
+      });
+
       this.requestUpdate();
     } catch (error) {
       console.error('Error loading checkboxes:', error);
-      console.error(this.DynamicCheckBoxVal);
+      console.log(this.DynamicCheckBoxVal);
     }
   }
 
   render() {
-    return html`
-      ${this.DynamicCheckBoxVal}
-      <div class="checkbox-container">
-        ${this.checkboxes.map(
-          (checkbox) => html`
-            <div class="checkbox-item">
-              <input type="checkbox" class="checkbox-input" id="${checkbox.id}" .checked="${checkbox.checked}" />
-              <label class="checkbox-label" for="${checkbox.id}">${checkbox.label}</label>
-            </div>
-          `
-        )}
-      </div>
-    `;
+    return html` <b>${this.DataStructure}</b> `;
   }
 }
